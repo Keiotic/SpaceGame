66617551f41427eb21e0e8d1464ffb84c5f5fa6e
Keiotic <54135223+Keiotic@users.noreply.github.com>
2021-02-16 00:11:55 +0100

    Updated character class
    Still got a few issues regarding collisions to fix.

--- Assets/Scripts/Player/TopDownPlayerController.cs
+++ Assets/Scripts/Player/TopDownPlayerController.cs
@@ -8,36 +8,43 @@ public class TopDownPlayerController : MonoBehaviour
     public float speedInterpolation = 10;
     private Rigidbody2D rigidbody;
     private Vector2 movementVector;
+    private Vector4 movementConstraints;
+    private BoxCollider2D footBox;
     public bool canMove = true;
 
     public GameObject ship;
-    public Vector2 shipOffset;
 
     void Start()
     {
         if (!rigidbody) rigidbody = this.GetComponent<Rigidbody2D>();
+        footBox = this.GetComponent<BoxCollider2D>();
+        if (IsOnShip())
+        {
+            transform.parent = ship.transform;
+        }
     }
 
     public void Update()
     {
+
         Vector2 targetVector = new Vector2();
         if (canMove)
         {
-            targetVector = Input.GetAxisRaw("Horizontal") * transform.right + Input.GetAxisRaw("Vertical") * transform.up;
+            if (IsOnShip())
+            {
+                targetVector = Input.GetAxisRaw("Horizontal") * Vector2.right + Input.GetAxisRaw("Vertical") * Vector2.up;
+            }
+            else
+            {
+                targetVector = Input.GetAxisRaw("Horizontal") * transform.right + Input.GetAxisRaw("Vertical") * transform.up;
+            }
         }
         movementVector = Vector2.Lerp(movementVector, targetVector, Time.deltaTime * speedInterpolation);
     }
     private void FixedUpdate()
     {
-        if (IsOnShip())
-        {
-            transform.rotation = ship.transform.rotation;
-            rigidbody.velocity = ship.GetComponent<Rigidbody2D>().velocity + movementSpeed * movementVector;
-        }
-        else
-        {
-            rigidbody.MovePosition(rigidbody.position + movementSpeed * movementVector * Time.fixedDeltaTime);
-        }
+        SetMovementConstraints();
+        DoMovement();
     }
 
     public void EnterShip(GameObject ship)
@@ -45,12 +52,21 @@ public class TopDownPlayerController : MonoBehaviour
         this.ship = ship;
     }
 
-    public void LeaveShip()
+    public void DoMovement()
     {
-     
+        if (IsOnShip())
+        {
+            transform.rotation = ship.transform.rotation;
+        }
+        transform.Translate(movementSpeed*Time.deltaTime*movementVector);
     }
 
-    public bool IsOnShip ()
+    public void LeaveShip()
+    {
+
+    }
+
+    public bool IsOnShip()
     {
         return ship != null;
     }
@@ -59,6 +75,12 @@ public class TopDownPlayerController : MonoBehaviour
     public void SetMobility(bool value)
     {
         canMove = value;
+    }
+    
+
+    public void SetMovementConstraints ()
+    {
+
     }
 
 }
